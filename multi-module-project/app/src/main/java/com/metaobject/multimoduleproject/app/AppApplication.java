4e125c088a450e18697f82ab38f3e71526bba342
Feng Chen <chenf@outlook.com>
2020-01-20 17:45:44 +0800

    autowire bean from library project

--- multi-module-project/app/src/main/java/com/metaobject/multimoduleproject/app/AppApplication.java
+++ multi-module-project/app/src/main/java/com/metaobject/multimoduleproject/app/AppApplication.java
@@ -1,9 +1,14 @@
 package com.metaobject.multimoduleproject.app;
 
+import com.metaobjectmultimoduleproject.service.UserService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(UserService.class)
 public class AppApplication {
 
     public static void main(String[] args) {
--- multi-module-project/app/src/main/java/com/metaobject/multimoduleproject/app/MainController.java
+++ multi-module-project/app/src/main/java/com/metaobject/multimoduleproject/app/MainController.java
@@ -2,6 +2,7 @@ package com.metaobject.multimoduleproject.app;
 
 import com.metaobjectmultimoduleproject.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,15 +11,14 @@ public class MainController {
 
     private UserService userService;
 
-//    @Autowired
-//    public MainController(UserService userService) {
-//        this.userService = userService;
-//    }
+    @Autowired
+    public MainController(UserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping("/admin")
     public User showDefaultAdmin() {
         User user = new User();
-        userService = new UserService();
         user.setName(userService.getDefaultAdminName());
         return user;
     }
